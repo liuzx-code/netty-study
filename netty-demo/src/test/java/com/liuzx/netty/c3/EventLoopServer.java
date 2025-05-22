33d855bec364e20dd29eef7021aa98b00ef618b0
liuzx-code <1091574678@qq.com>
2025-05-22 23:10:16 +0800

    update EventLoopGroup 进行再次细分工作

--- netty-demo/src/test/java/com/liuzx/netty/c3/EventLoopServer.java
+++ netty-demo/src/test/java/com/liuzx/netty/c3/EventLoopServer.java
@@ -2,9 +2,7 @@ package com.liuzx.netty.c3;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -16,7 +14,10 @@ import java.nio.charset.Charset;
 @Slf4j
 public class EventLoopServer {
     public static void main(String[] args) {
+        // 细分2
+        EventLoopGroup group = new DefaultEventLoopGroup();
         new ServerBootstrap()
+                // 细分1：
                 // 添加两个 EventLoopGroup boss 和 worker
                 // boss 只负责 ServerSocketChannel 上 accept 事件
                 // worker 只负责 SocketChannel 上读写事件
@@ -26,7 +27,15 @@ public class EventLoopServer {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
 //                        ch.pipeline().addLast(new StringEncoder());
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        ch.pipeline().addLast("handler1",new ChannelInboundHandlerAdapter() {
+
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug("msg: " + buf.toString(Charset.defaultCharset()));
+                                ctx.fireChannelRead(msg); // 让消息传递给下一个 handler 进行处理
+                            }
+                        }).addLast(group,"handler2",new ChannelInboundHandlerAdapter() {
 
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
